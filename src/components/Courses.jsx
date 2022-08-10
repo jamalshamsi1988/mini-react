7701f40023376b67d5c1e3155bf20c4428e3af69
jamalshamsi <jamal.shamsi1988@gmail.com>
2022-08-10 21:52:46 +0430

    first app

---
+++ src/components/Courses.jsx
@@ -0,0 +1,14 @@
+import HTML from "../assets/img/HTML.png"
+
+const Courses = () => {
+    return ( 
+    <section className="courses-box">
+     <h3>HTML-Courses</h3> 
+    <h4>HTML</h4>
+    <img src={HTML}/> 
+        </section>
+  
+    );
+}
+ 
+export default Courses;
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+
+
+const Header = () => {
+    return ( 
+        <header>
+            <ul>
+                <li>Home</li>
+                <li>Blog</li>
+                <li>About</li>
+            </ul>
+        </header>
+     );
+}
+ 
+export default Header;
