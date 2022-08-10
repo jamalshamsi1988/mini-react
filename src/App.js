277f33d3d8b69947a85e5e01ede70f04332c75cc
jamalshamsi <jamal.shamsi1988@gmail.com>
2022-08-11 02:37:08 +0430

    add style jsx

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import Header from "./components/Header";
 import "./assets/styles/index.css";
 import Courses from "./components/Courses";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Header />
       <article className="wrapper">
-        <Courses />
-        <Courses />
-        <Courses />
+        <Courses
+          courses="HTML"
+          course="Flex"
+          styles={{ backgroundColor: "gold" }}
+        />
+        <Courses
+          courses="HTML"
+          course="Grid"
+          styles={{ backgroundColor: "green" }}
+        />
+        <Courses
+          courses="HTML"
+          course="Sematic-Tags"
+          styles={{ backgroundColor: "blue" }}
+        />
       </article>
+      <Footer> This is my first site by React. </Footer>
     </>
   );
 }
--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -1,14 +1,13 @@
 import HTML from "../assets/img/HTML.png"
 
-const Courses = () => {
-    return ( 
-    <section className="courses-box">
-     <h3>HTML-Courses</h3> 
-    <h4>HTML</h4>
-    <img src={HTML}/> 
-        </section>
-  
-    );
-}
+const Courses = ({courses, course, styles}) => {
+  return (
+    <section className="courses-box" style={ styles }>
+      <h3>{courses}</h3>
+      <h4>{course}</h4>
+      <img src={HTML} />
+    </section>
+  );
+};
  
 export default Courses;
